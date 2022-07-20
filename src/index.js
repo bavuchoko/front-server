75ebb7174d1fc47c2a89e0d20196b68f01cb9677
bavuchoko <bavuchoko@naver.com>
2022-07-20 17:07:02 +0900

    a

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import Root from './Root';
+// import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import configureStore from './redux/configureStore';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const store = configureStore();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const render = Component => {
+    ReactDOM.render(
+            <Component store={store}/>,
+
+        document.getElementById('root')
+    );
+};
+
+render(Root);
+
+if(module.hot) {
+    module.hot.accept('./Root', () => render(Root));
+}
+
+// registerServiceWorker();
