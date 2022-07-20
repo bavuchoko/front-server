af9e5af82d98944f343294d71d475ecee8abd67f
pjs <bavuchoko@naver.com>
2022-07-20 21:20:28 +0900

    login

--- src/components/Auth/InputWithLabel.js
+++ src/components/Auth/InputWithLabel.js
@@ -16,14 +16,17 @@ const Label = styled.div`
 `;
 
 const Input = styled.input`
-    width: 100%;
-    border: 1px solid ${oc.gray[3]};
-    outline: none;
-    border-radius: 0px;
-    line-height: 2.5rem;
-    font-size: 1.2rem;
-    padding-left: 0.5rem;
-    padding-right: 0.5rem;
+  width: 100%;
+  border: 1px solid ${oc.gray[3]};
+  outline: none;
+  border-radius: 0px;
+  line-height: 2.5rem;
+  font-size: 1.2rem;
+  padding-left: 0.5rem;
+  padding-right: 0.5rem;
+  ::placeholder {
+    color: ${oc.gray[3]};
+  }
 `;
 
 // rest 쪽에는 onChange, type, name, value, placeholder 등의 input 에서 사용 하는 값들을 넣어줄수 있다.
