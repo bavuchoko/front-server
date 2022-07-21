1697365d5a500547e717a29bd04dcfe7debe0d80
bavuchoko <bavuchoko@naver.com>
2022-07-21 11:48:25 +0900

    ddd

--- src/lib/styleUtils.js
+++ src/lib/styleUtils.js
@@ -1,4 +1,4 @@
-import { css } from 'styled-components';
+import { css, keyframes  } from 'styled-components';
 
 // 미디어 쿼리 헬퍼: https://www.styled-components.com/docs/advanced#media-templates 참조
 export const sizes = {
@@ -7,7 +7,25 @@ export const sizes = {
     tablet: '768px',
     phone: '376px'
 };
-
+export const transitions = {
+    shake: keyframes`
+        0% {
+            transform: translate(-30px);
+        }
+        25% {
+            transform: translate(15px);
+        }
+        50% {
+            transform: translate(-10px);
+        }
+        75% {
+            transform: translate(5px);
+        }
+        100% {
+            transform: translate(0px);
+        }
+    `
+};
 export const media = Object.keys(sizes).reduce((acc, label) => {
     acc[label] = (...args) => css`
         @media (max-width: ${sizes[label]}) {
