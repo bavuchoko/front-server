920b7bd424fdc4ca58d2ac38b76427b8ab1b9522
bavuchoko <bavuchoko@naver.com>
2022-07-29 17:23:31 +0900

    a

--- src/components/sideMenu/HomeSideMenu.js
+++ src/components/sideMenu/HomeSideMenu.js
@@ -8,6 +8,7 @@ export default function Sidebar() {
         <>
             <nav className="height-100per bac-color-grey border-1-s-g">
                 <div className="width-100per height-55p bac-color-FEB139 color-FEB139 tc-lh60-fs15">
+                    <input className="search-bar" placeholder="검색..."/>
                     <div className="float-right height-100per width-70p"><img className="hover-btn" id="magnifier_img" src={magnifier2} /></div>
                 </div>
                     <div  className="">
--- src/containers/Home/Main.js
+++ src/containers/Home/Main.js
@@ -36,13 +36,13 @@ class Main extends Component {
                                         <img className="article-card-img" src=""/>
                                     </div>
                                     <div>
-                                        <span>JAVA</span>
-                                        <span>2022-01-01</span>
+                                        <span className="article-card-category">JAVA</span>
+                                        <span className="article-card-date">2022-01-01</span>
                                     </div>
-                                    <p>
+                                    <p className="article-card-title">
                                         React 리덕스(Redux) Immutable.js 상태관리 & Ducks 파일 구조
                                     </p>
-                                    <p>
+                                    <p className="article-card-content">
                                         자바스크립트에서 불변성 데이터를 다룰 수 있도록 도와주는 것이 바로 Immutable.js입니다. 우선, 객체 불변성에 대해 살펴 보도록 하겠습니다. 12345let a = 5;let b ...
                                     </p>
 
@@ -51,19 +51,21 @@ class Main extends Component {
 
                                 <Link className="article-card hover-btn" to="/">
                                     <div className="width-100per height-230p bac-color-white2">
+                                        <img className="article-card-img" name="search" src=""/>
                                     </div>
                                     <div>
-                                        <span>JAVA</span>
-                                        <span>2022-01-01</span>
+                                        <span className="article-card-category">JAVA</span>
+                                        <span className="article-card-date">2022-01-01</span>
                                     </div>
-                                    <p>
+                                    <p className="article-card-title">
                                         React 리덕스(Redux) Immutable.js 상태관리 & Ducks 파일 구조
                                     </p>
-                                    <p>
+                                    <p className="article-card-content">
                                         자바스크립트에서 불변성 데이터를 다룰 수 있도록 도와주는 것이 바로 Immutable.js입니다. 우선, 객체 불변성에 대해 살펴 보도록 하겠습니다. 12345let a = 5;let b ...
                                     </p>
 
                                 </Link>
+
                             </li>
 
                             </ul>
