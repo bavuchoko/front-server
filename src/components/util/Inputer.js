9753bd699199e75115a06c37e951618c172e2a87
pjs <bavuchoko@naver.com>
2022-07-29 21:05:54 +0900

    input

--- src/components/util/Inputer.js
+++ src/components/util/Inputer.js
@@ -1,17 +1,27 @@
 
-import magnifier2 from "../../assets/image/magnifier-white.png";
-import React from "react";
+import x from "../../assets/image/x.png";
+import magnifierWhite from "../../assets/image/magnifier-white.png";
+import React,{useState} from "react";
 
 
 function Inputer () {
 
+    const [visible, setVisible] = useState(false);
+    const clickedToggle = () => {
+       setVisible(!visible);
 
+    };
+    const magnifierIcon="magnifierIcon";
+    const roateXIcon= "roateXIcon";
+    const show="search-bar show";
+    const liner="search-bar liner";
     return (
 
         <div className="width-100per height-55p bac-color-FEB139 color-FEB139 tc-lh60-fs15">
-            <input className="search-bar" placeholder="검색..."/>
-            <div className="float-right height-100per width-70p"><img className="hover-btn" id="magnifier_img" src={magnifier2} /></div>
+            <input className={visible ? show :liner}  placeholder="search..." />
+            <div className="float-right height-100per width-70p"  onClick={clickedToggle}><img className="" id="magnifier_img" src={ visible ? x :magnifierWhite }  /></div>
         </div>
+
 );
 
 }
