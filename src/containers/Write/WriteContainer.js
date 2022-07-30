2320d076414c0cff7251c1ca3e8ca40a55c8cf45
pjs <bavuchoko@naver.com>
2022-07-30 22:41:56 +0900

    text editor

--- src/containers/Write/WriteContainer.js
+++ src/containers/Write/WriteContainer.js
@@ -1,22 +1,51 @@
-import React from 'react';
-import HomeSideMenu from "../../components/sideMenu/HomeSideMenu";
+import React, {createRef} from 'react';
 import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
 import * as authActions from "../../redux/modules/auth";
 import * as userActions from "../../redux/modules/user";
 import {Link} from "react-router-dom";
+import '@toast-ui/editor/dist/toastui-editor.css';
+import {Editor} from '@toast-ui/react-editor';
+import Prism from 'prismjs';
+import 'prismjs/themes/prism.css';
+import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
+import codeSyntaxHighlightPlugin from "@toast-ui/editor-plugin-code-syntax-highlight";
+import 'tui-color-picker/dist/tui-color-picker.css';
+import colorPlugin from '@toast-ui/editor-plugin-color-syntax';
+
+
 
 function WriteContainer(props) {
     let isLoggedIn = props.isLoggedIn;
     isLoggedIn = true;
     const writeBtn = isLoggedIn? <Link className="roboto write_btn float-right" to="/write">WRITE</Link> : null;
 
+    const editorRef = createRef();
+    const onChangeEditorTextHandler =()=>{
+        console.log(editorRef.current.getInstance().getMarkdown());
+    }
 
 
     return (
         <div className="width-1248px mar-auto-0 disp-flex height-100vh">
-            <div className="width-100per-350p mar-auto-0 bac-color-white">
+            <div className="width-1168px mar-auto-0 bac-color-white padding-rl-40p bac-color-temp">
+                <div className="editor-header-container">
+                    헤더 배너 영역
+                </div>
+                <div className="editor-title-container">
 
+                    제목과 등록버튼이 들어갈영역
+                </div>
+                <div>
+
+                    <Editor
+                        initialValue="hello react editor world!"
+                        previewStyle="vertical"
+                        height="600px"
+                        initialEditType="markdown"
+                        useCommandShortcut={true}
+                    />
+                </div>
             </div>
         </div>
     );
