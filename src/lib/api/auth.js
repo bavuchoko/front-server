f538c94855dce41acfb59e869168d47079c625e0
pjs <bavuchoko@naver.com>
2022-08-07 01:13:18 +0900

    cors

--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -15,7 +15,7 @@ export const userRegister = ({username, password}) => {
 }
 export const userLogin = ({username, password}) => {
     console.log("userLogin");
-    axios.post('/api/user/authenticate', { username, password })
+    axios.post('http://localhost:8080/api/user/authenticate', { username, password })
 
         .then(
             res => {
